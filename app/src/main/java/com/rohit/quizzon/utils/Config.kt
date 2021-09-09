ec8bd595b3fb9d1d2d8303be264f2671cec148db
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-09-09 11:24:59 +0530

    Fixed Issue in create quiz fragment. Fixed Issue in profile: not showing quiz created by user.

--- app/src/main/java/com/rohit/quizzon/utils/Config.kt
+++ app/src/main/java/com/rohit/quizzon/utils/Config.kt
@@ -1,8 +1,10 @@
 package com.rohit.quizzon.utils
 
+import com.rohit.quizzon.BuildConfig
+
 class Config {
     companion object {
-        const val BASE_URL = "https://quiz-rohitjakhar.harperdbcloud.com"
+        const val BASE_URL = BuildConfig.BASE_URL
         var currentLanguage = "en"
     }
 }
